f34e731d3a125219067093ba6f7415c9c2f95db9
Krzysztof Madej <krzysztof.madej@hotmail.com>
2020-04-16 14:21:09 +0200

    Task 8 Added fetching user name profile from gravatar service

--- Todo/Services/Gravatar.cs
+++ Todo/Services/Gravatar.cs
@@ -1,10 +1,23 @@
-﻿using System.Security.Cryptography;
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Todo.Services
 {
     public static class Gravatar
     {
+        private static readonly HttpClient client = new HttpClient();
+
+        static Gravatar()
+        {
+            // Idally it would be to apply some kind of policy how to handle unavailable service
+            // Polly can do the right job here
+            client.Timeout = TimeSpan.FromSeconds(2);
+        }
+
         public static string GetHash(string emailAddress)
         {
             using (var md5 = MD5.Create())
@@ -19,6 +32,30 @@ namespace Todo.Services
                 }
                 return builder.ToString().ToLowerInvariant();
             }
+        }
+
+        public static string GetUserName(string hash)
+        {
+            var gravatarProfileUrl = $"https://www.gravatar.com/{hash}.json";
+
+            try
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Add("User-Agent", "Storm Ideas rules the world");
+
+                HttpResponseMessage response = client.GetAsync(gravatarProfileUrl).Result;
+                response.EnsureSuccessStatusCode();
+                string responseBody = response.Content.ReadAsStringAsync().Result;
+                GravatarProfile p = JsonConvert.DeserializeObject<GravatarProfile>(responseBody);
+
+                return p.Entry.FirstOrDefault()?.DisplayName;
+            }
+            catch (HttpRequestException e)
+            {
+                // Log exception
+            }
+
+            return string.Empty;
         }
     }
 }
---
+++ Todo/Services/GravatarProfile.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Classes generated with https://app.quicktype.io/#l=cs&r=json2csharp
+
+namespace Todo.Services
+{
+    public partial class GravatarProfile
+    {
+        [JsonProperty("entry")]
+        public Entry[] Entry { get; set; }
+    }
+
+    public partial class Entry
+    {
+        [JsonProperty("id")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Id { get; set; }
+
+        [JsonProperty("hash")]
+        public string Hash { get; set; }
+
+        [JsonProperty("requestHash")]
+        public string RequestHash { get; set; }
+
+        [JsonProperty("profileUrl")]
+        public Uri ProfileUrl { get; set; }
+
+        [JsonProperty("preferredUsername")]
+        public string PreferredUsername { get; set; }
+
+        [JsonProperty("thumbnailUrl")]
+        public Uri ThumbnailUrl { get; set; }
+
+        [JsonProperty("photos")]
+        public Photo[] Photos { get; set; }
+
+        [JsonProperty("name")]
+        public Name Name { get; set; }
+
+        [JsonProperty("displayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("urls")]
+        public object[] Urls { get; set; }
+    }
+
+    public partial class Name
+    {
+        [JsonProperty("givenName")]
+        public string GivenName { get; set; }
+
+        [JsonProperty("familyName")]
+        public string FamilyName { get; set; }
+
+        [JsonProperty("formatted")]
+        public string Formatted { get; set; }
+    }
+
+    public partial class Photo
+    {
+        [JsonProperty("value")]
+        public Uri Value { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    }
+}
